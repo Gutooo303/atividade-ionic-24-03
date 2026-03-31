933813cf6e6a9e568a7248dba3cffbd7857d548c
Augusto Gabriel de Almeida Santos <augusto.031109@gmail.com>
2026-03-31 12:21:15 +0000

    Upload dos arquivos da atividade.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'atividade_pam',
+  webDir: 'www'
+};
+
+export default config;
